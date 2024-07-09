0fc17efe03c091eb8045592eb1a97eb447286b9c
alfarseniko <alfarseniko@gmail.com>
2024-07-09 09:11:05 +0500

    started chapter 4

---
+++ rustBook/chapter4/_1_memoryAndAllocation/target/debug/_1_memoryAndAllocation.d
@@ -0,0 +1,1 @@
+/home/haris/learningRust/rustBook/chapter4/_1_memoryAndAllocation/target/debug/_1_memoryAndAllocation: /home/haris/learningRust/rustBook/chapter4/_1_memoryAndAllocation/src/main.rs
---
+++ rustBook/chapter4/_1_memoryAndAllocation/target/debug/deps/_1_memoryAndAllocation-85a4f63f6e243d79.d
@@ -0,0 +1,5 @@
+/home/haris/learningRust/rustBook/chapter4/_1_memoryAndAllocation/target/debug/deps/_1_memoryAndAllocation-85a4f63f6e243d79: src/main.rs
+
+/home/haris/learningRust/rustBook/chapter4/_1_memoryAndAllocation/target/debug/deps/_1_memoryAndAllocation-85a4f63f6e243d79.d: src/main.rs
+
+src/main.rs:
